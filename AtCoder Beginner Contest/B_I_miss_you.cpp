5703a77a28427b9dd9e509d5f77d0415a2e5b04a
FLRYG <flryg1113169@gmail.com>
2020-03-04 23:59:34 +0900

    2020.03.04

---
+++ AtCoder Beginner Contest/B_I_miss_you.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    char* S;
+    std::cin>>S;
+    
+    char X[]
+    for(int i=0;strlen(S)-1;i++){
+        *(S+i)='x';
+    }
+
+    std::cout<<S;
+}
--- AtCoder Beginner Contest/C_Poll.cpp
+++ AtCoder Beginner Contest/C_Poll.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <map>
+#include <string>
 
 int main(){
     int N;
     char* S;
-    
+    std::cin>>N;
+    S=new char[N];
+    for(int i=0;i<N;i++){
+        std::cin>>S[i];
+        //std::cout<<S[i];
+    }
+
+    delete[] S;
 }
