b95e9d259da6fb64fc581482d4f5263a3bae2f5e
FLRYG <flryg1113169@gmail.com>
2020-06-07 23:58:37 +0900

    2020.06.07

---
+++ ABC/A_Remaining Time.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <list>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
+#define repr(e,x) for(auto& e:x)
+using namespace std;
+typedef long long ll;
+
+int A,B;
+
+int main(){
+    cin>>A>>B;
+
+    cout<<(A+B)%24<<endl;
+}
