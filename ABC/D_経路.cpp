38389d8b97be70f1211565b1579a8b0ebdf42c7f
FLRYG <flryg1113169@gmail.com>
2020-10-04 23:04:05 +0900

    2020.10.04

--- ABC/D_経路.cpp
+++ ABC/D_経路.cpp
@@ -15,7 +15,8 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-//typedef pair<int,int> P;
+typedef pair<int,int> P;
+typedef pair<int,P> PP;
 int const INF=1001001001;
 ll const LINF=1001001001001001001;
 ll const MOD=1000000007;
@@ -31,9 +32,35 @@ int main(){
     rep(i,H) rep(j,W) cin>>a[i][j];
 
     vector<vector<int>> cnt(H,vector<int>(W,1));
+    priority_queue<PP> q;
     rep(i,H){
         rep(j,W){
-            
+            q.push(PP(a[i][j],P(i,j)));
         }
     }
+
+    while(!q.empty()){
+        PP pp=q.top(); q.pop();
+        int num=pp.first;
+        int x=pp.second.first;
+        int y=pp.second.second;
+        rep(k,4){
+            int i=x+I[k];
+            int j=y+J[k];
+            if(0<=i && i<H && 0<=j && j<W && a[i][j]<num){
+                cnt[i][j]+=cnt[x][y];
+                cnt[i][j]%=MOD;
+            }
+        }
+    }
+
+    int ans=0;
+    rep(i,H){
+        rep(j,W){
+            ans+=cnt[i][j];
+            ans%=MOD;
+        }
+    }
+
+    cout<<ans<<endl;
 }
