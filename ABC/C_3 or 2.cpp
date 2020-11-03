22686b51154705f444ac45bbd8975e257398d4c3
FLRYG <flryg1113169@gmail.com>
2020-11-03 23:55:19 +0900

    2020.11.03

---
+++ ABC/C_3 or 2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
+#define repr(e,x) for(auto& e:x)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+//typedef pair<int,int> P;
+double const PI=3.141592653589793;
+int const INF=1001001001;
+ll const LINF=1001001001001001001;
+ll const MOD=1000000007;
+
+ll N;
+ll a[10000];
+
+int main(){
+    cin>>N;
+    rep(i,N) cin>>a[i];
+
+    ll ans=0;
+    rep(i,N){
+        while(a[i]%2==0){
+            a[i]/=2;
+            ans++;
+        }
+    }
+
+    cout<<ans<<endl;
+}
--- library/binary_search.cpp
+++ library/binary_search.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std;
 
+int N;
+int a[100];
+int x;
 
-int binary_search(int val, int l, int r){
-    while(r-l>1){
-        int mid=(l+r)/2;
+void binary_search(){
+    int l=-1;
+    int r=N;
+    while(r-l>1) {
+        int mid=l+(r-l)/2;
+        if (a[mid]>=x) r=mid;
+        else l=mid;
     }
 }
