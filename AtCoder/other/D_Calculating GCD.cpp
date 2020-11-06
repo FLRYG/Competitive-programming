14e946ca8e95bb499b5ed2318083ce59e5c17aba
FLRYG <flryg1113169@gmail.com>
2020-11-06 23:59:05 +0900

    2020.11.06

---
+++ AtCoder/other/D_Calculating GCD.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <queue>
+#include <deque>
+#include <map>
+#include <unordered_map>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
+#define repr(e,x) for(auto& e:x)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+//typedef pair<int,int> P;
+double const PI=3.141592653589793;
+int const INF=1001001001;
+ll const LINF=1001001001001001001;
+ll const MOD=1000000007;
+
+template<class T>T gcd(T a, T b){return b?gcd(b,a%b):a;}
+template<class T>T lcm(T a, T b){return a/gcd(a,b)*b;}
+
+int N,Q;
+int A[100000];
+int S[100000];
+
+int main(){
+    cin>>N>>Q;
+    rep(i,N) cin>>A[i];
+    rep(i,Q) cin>>S[i];
+
+    int g=0;
+    rep(i,N) g=gcd(g,A[i]);
+
+    
+    
+    return 0;
+}
