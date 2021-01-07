5095249145c369bb8ed54b9ddd1bf35dea14e8f4
FLRYG <flryg1113169@gmail.com>
2021-01-07 23:50:23 +0900

    2020.01.07

--- AtCoder/ABC/E_Roaming.cpp
+++ AtCoder/ABC/E_Roaming.cpp
@@ -22,26 +22,43 @@ int const INF=1001001001;
 ll const LINF=1001001001001001001;
 ll const MOD=1000000007;
 
-ll N,K;
-map<P,ll> memo;
+ll mpow(ll x,ll n){
+    if(n==0) return 1;
+    else if(n%2) return x*mpow(x,n-1)%MOD;
+    return mpow(x*x%MOD,n/2)%MOD;
+}
 
-ll dfs(ll n, ll k){
-    cout<<n<<' '<<k<<endl;
-    if(n==1) return 1;
-    if(memo[P(n,k)]!=0) return memo[P(n,k)];
-    ll res=0;
-    rep(i,min(n,k+1)+1){
-        res+=dfs(n/2,i)+dfs((n+1)/2,min(n,k+1)-i);
-        res%=MOD;
-    }
-    memo[P(n,k)]=res;
+ll mfrac(ll n){
+    if(n==0) return 1;
+    else return n*mfrac(n-1)%MOD;
+}
+
+ll mperm(ll n,ll r){
+    ll res=1;
+    for(ll i=0;i<r;i++) res=res*(n-i)%MOD;
     return res;
 }
 
+ll mcomb(ll n,ll r){
+    if(r>n-r) r=n-r;
+    ll res=mperm(n,r);
+    return res*mpow(mfrac(r),MOD-2)%MOD;
+}
+
+ll N,K;
+
 int main(){
     cin>>N>>K;
-
-    cout<<dfs(N,K)<<endl;
     
-    return 0;
+    ll ans=1;
+    ll m1=1, m2=1;
+    repn(k,min(N-1,K)){
+        m1=m1*(N-k+1)%MOD*mpow(k,MOD-2)%MOD;
+        m2=m2*(N-k)%MOD*mpow(k,MOD-2)%MOD;
+        ans+=m1*m2;
+        ans%=MOD;
+    }
+    if(ans<0) ans+=MOD;
+
+    cout<<ans<<endl;
 }
