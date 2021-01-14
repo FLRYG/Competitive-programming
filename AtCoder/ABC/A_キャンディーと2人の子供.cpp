87cb9ee6090393c4848f8ff5e6498c58db64d855
FLRYG <flryg1113169@gmail.com>
2021-01-14 23:26:19 +0900

    2021.01.14

---
+++ AtCoder/ABC/A_キャンディーと2人の子供.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
+#define repr(e,x) for(auto& e:x)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+//typedef pair<int,int> P;
+double const PI=3.141592653589793;
+int const INF=1001001001;
+ll const LINF=1001001001001001001;
+ll const MOD=1000000007;
+
+int a,b,c;
+
+int main(){
+    cin>>a>>b>>c;
+
+    string ans="No";
+    if(a+b==c || b+c==a || c+a==b) ans="Yes";
+
+    cout<<ans<<endl;
+    
+    return 0;
+}
