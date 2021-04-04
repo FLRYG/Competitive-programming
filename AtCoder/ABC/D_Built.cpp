4f8937e6770d6b57f0cff928088e4efccffd966e
FLRYG <flryg1113169@gmail.com>
2021-04-04 23:50:42 +0900

    2021.04.04

--- AtCoder/ABC/D_Built.cpp
+++ AtCoder/ABC/D_Built.cpp
@@ -21,7 +21,7 @@
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-// typedef pair<int,int> P;
+typedef pair<ll,ll> P;
 // typedef pair<int,P> IP;
 // typedef pair<P,P> PP;
 double const PI=3.141592653589793;
@@ -29,34 +29,14 @@ int const INF=1001001001;
 ll const LINF=1001001001001001001;
 ll const MOD=1000000007;
 
-ll mpow(ll x,ll n){
-    if(n==0) return 1;
-    else if(n%2) return x*mpow(x,n-1)%MOD;
-    return mpow(x*x%MOD,n/2)%MOD;
-}
-
-string L;
-int N;
-
-ll f(ll i, bool prev, ll one){
-    if(prev && i<N){
-        if(L[i]=='0'){
-            return f(i+1,true,one)%MOD;
-        }else{
-            return (f(i+1,false,one)+f(i+1,true,one+1))%MOD;
-        }
-    }else{
-        return mpow(2,one)*mpow(3,N-i)%MOD;
-    }
-}
+ll N;
+P xy[100000];
 
 int main(){
-    cin>>L;
-    N=L.size();
+    cin>>N;
+    rep(i,N) cin>>xy[i].first>>xy[i].second;
 
-    ll ans=f(0,true,0);
-
-    cout<<ans<<endl;
+    
     
     return 0;
 }
