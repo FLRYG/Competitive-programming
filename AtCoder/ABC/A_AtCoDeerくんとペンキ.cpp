e6bc7b1de66bc5f19fb7c1590a7ca151eb962ae8
FLRYG <flryg1113169@gmail.com>
2021-04-29 23:38:31 +0900

    2021.04.29

--- AtCoder/ABC/A_AtCoDeerくんとペンキ.cpp
+++ AtCoder/ABC/A_AtCoDeerくんとペンキ.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <list>
 #include <iomanip>
 #include <stack>
 #include <queue>
@@ -11,22 +12,34 @@
 #include <set>
 #include <map>
 #include <unordered_map>
+#include <bitset>
+#include <chrono>
+#include <random>
 #define rep(i,n) for(int i=0;i<n;i++)
 #define repn(i,n) for(int i=1;i<=n;i++)
 #define repr(e,x) for(auto& e:x)
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-//typedef pair<int,int> P;
+// typedef pair<int,int> P;
+// typedef pair<int,P> IP;
+// typedef pair<P,P> PP;
 double const PI=3.141592653589793;
 int const INF=1001001001;
 ll const LINF=1001001001001001001;
 ll const MOD=1000000007;
 
-int N=6, M=15;
+int a,b,c;
 
 int main(){
-    
+    cin>>a>>b>>c;
+
+    set<int> s;
+    s.insert(a);
+    s.insert(b);
+    s.insert(c);
+
+    cout<<s.size()<<endl;
     
     return 0;
 }
