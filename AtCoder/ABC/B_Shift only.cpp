6bf655894fd79cc3c1c26277d48690b1387f400a
FLRYG <flryg1113169@gmail.com>
2021-05-28 23:38:08 +0900

    2021.05.28

---
+++ AtCoder/ABC/B_Shift only.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <list>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+#include <chrono>
+#include <random>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
+#define repr(e,x) for(auto& e:x)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+// typedef pair<int,int> P;
+// typedef pair<int,P> IP;
+// typedef pair<P,P> PP;
+double const PI=3.141592653589793;
+int const INF=1001001001;
+ll const LINF=1001001001001001001;
+ll const MOD=1000000007;
+
+template<class T>T gcd(T a, T b){return b?gcd(b,a%b):a;}
+template<class T>T lcm(T a, T b){return a/gcd(a,b)*b;}
+
+int N;
+
+int main(){
+    cin>>N;
+    int g=0;
+    rep(i,N){
+        int a;
+        cin>>a;
+        g=gcd(g,a);
+    }
+
+    int ans=0;
+    while(g%2==0){
+        ans++;
+        g/=2;
+    }
+
+    cout<<ans<<endl;
+    
+    return 0;
+}
