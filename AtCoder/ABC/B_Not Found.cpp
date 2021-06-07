1877a9ce2e721ff048e4c542fc8d128e14aa8c82
FLRYG <flryg1113169@gmail.com>
2021-06-07 23:56:02 +0900

    2021.06.07

---
+++ AtCoder/ABC/B_Not Found.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <list>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+#include <chrono>
+#include <random>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
+#define repr(e,x) for(auto& e:x)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+// typedef pair<int,int> P;
+// typedef pair<int,P> IP;
+// typedef pair<P,P> PP;
+double const PI=3.141592653589793;
+int const INF=1001001001;
+ll const LINF=1001001001001001001;
+ll const MOD=1000000007;
+
+string S;
+
+int main(){
+    cin>>S;
+    int N=S.size();
+
+    vector<int> cnt(26,0);
+    rep(i,N){
+        cnt[S[i]-'a']=1;
+    }
+
+    string ans="None";
+    rep(i,26){
+        if(cnt[i]==0){
+            ans=i+'a';
+            break;
+        }
+    }
+
+    cout<<ans<<endl;
+    
+    return 0;
+}
