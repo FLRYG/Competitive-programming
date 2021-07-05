bbdaf750f05db490560ace9a32cef1936ae5442a
FLRYG <flryg1113169@gmail.com>
2021-07-05 23:30:01 +0900

    2021.07.05

--- AtCoder/ABC/B_OddString.cpp
+++ AtCoder/ABC/B_OddString.cpp
@@ -15,30 +15,30 @@
 #include <bitset>
 #include <chrono>
 #include <random>
-#define rep(i,n) for(ll i=0;i<n;i++)
-#define repn(i,n) for(ll i=1;i<=n;i++)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
 #define repr(e,x) for(auto& e:x)
 using namespace std;
 typedef long long ll;
 typedef long double ld;
-// typedef pair<ll,ll> P;
-// typedef pair<ll,P> IP;
+// typedef pair<int,int> P;
+// typedef pair<int,P> IP;
 // typedef pair<P,P> PP;
 double const PI=3.141592653589793;
-ll const INF=1001001001;
+int const INF=1001001001;
 ll const LINF=1001001001001001001;
 ll const MOD=1000000007;
 
-ll N,M;
-ll A[400];
-ll B[400];
-ll C[400];
+string S;
 
 int main(){
-    cin>>N>>M;
-    rep(i,M) cin>>A[i]>>B[i]>>C[i];
+    cin>>S;
+    int N=S.size();
 
-    
+    string ans;
+    rep(i,N) if(!(i&1)) ans+=S[i];
+
+    cout<<ans<<endl;
     
     return 0;
 }
