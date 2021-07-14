80670c00504c4a7064b391afb6d62a9a9b6adf41
FLRYG <flryg1113169@gmail.com>
2021-07-14 23:53:38 +0900

    2021.07.14

---
+++ AtCoder/other/045_Simple Grouping（★6）.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <list>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <bitset>
+#include <chrono>
+#include <random>
+#define rep(i,n) for(int i=0;i<n;i++)
+#define repn(i,n) for(int i=1;i<=n;i++)
+#define repr(e,x) for(auto& e:x)
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+// typedef pair<int,int> P;
+// typedef pair<int,P> IP;
+// typedef pair<P,P> PP;
+double const PI=3.141592653589793;
+int const INF=1001001001;
+ll const LINF=1001001001001001001;
+ll const MOD=1000000007;
+
+ll N,K;
+ll X[15];
+ll Y[15];
+
+int main(){
+    cin>>N>>K;
+    rep(i,N) cin>>X[i]>>Y[i];
+
+    vector<vector<ll>> dp(1<<N,vector<ll>(K+1,0));
+    repn(i,(1<<N)-1){
+        int cnt=0;
+        rep(j,N) cnt+=i&1;
+    }
+    
+    return 0;
+}
